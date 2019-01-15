2eec43ddf6eb3f58ab2d151a7546296f03871f78
Ran Proshan <ran.proshan@gmail.com>
2019-01-15 12:36:16 +0200

    code removal

--- Math/Setup.cpp
+++ Math/Setup.cpp
@@ -9,34 +9,6 @@
 
 #include <fstream>
 
-void write_online_setup(ofstream& outf, string dirname, const bigint& p, int lg2)
-{
-  if (p == 0)
-    throw runtime_error("prime cannot be 0");
-
-  stringstream ss;
-  ss << dirname;
-  cout << "Writing to file in " << ss.str() << endl;
-  // create preprocessing dir. if necessary
-  if (mkdir_p(ss.str().c_str()) == -1)
-  {
-    cerr << "mkdir_p(" << ss.str() << ") failed\n";
-    throw file_error();
-  }
-
-  // Output the data
-  ss << "/Params-Data";
-  outf.open(ss.str().c_str());
-  // Only need p and lg2 for online phase
-  outf << p << endl;
-  // Fix as a negative lg2 is a ``signal'' to choose slightly weaker
-  // LWE parameters
-  outf << abs(lg2) << endl;
-
-  gfp::init_field(p, true);
-  gf2n::init_field(lg2);
-}
-
 string get_prep_dir(int nparties, int lg2p, int gf2ndegree)
 {
   if (gf2ndegree == 0)
--- Math/Setup.h
+++ Math/Setup.h
@@ -8,8 +8,6 @@
 #ifndef MATH_SETUP_H_
 #define MATH_SETUP_H_
 
-#include "Math/bigint.h"
-
 #include <iostream>
 using namespace std;
 
@@ -20,9 +18,6 @@ using namespace std;
 /*
  * Routines to create and read setup files for the finite fields
  */
-
-// Create setup file for gfp and gf2n
-void write_online_setup(ofstream& outf, string dirname, const bigint& p, int lg2);
 
 // get main directory for prep. data
 string get_prep_dir(int nparties, int lg2p, int gf2ndegree);
